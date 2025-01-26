efe75c605870077c34355e59ca50e37f99cf045c
Cristianodhoni09 <amitkum9898@gmail.com>
2025-01-26 11:16:48 +0530

    add comments to productModel for better clarity on schema relationships and data types

--- models/productModel.js
+++ models/productModel.js
@@ -19,7 +19,7 @@ const productSchema = new mongoose.Schema(
       required: true,
     },
     category: {
-      type: mongoose.ObjectId, //getting the id of category created
+      type: mongoose.ObjectId, //getting the id of category created, it is a foreign key to establish the relationship between product and category collections
       ref: "Category", //Name we have kept in catogery schema
       required: true,
     },
@@ -28,8 +28,8 @@ const productSchema = new mongoose.Schema(
       required: true,
     },
     photo: {
-      data: Buffer, //Photo from mongodb, not from cloudinary/aws
-      contentType: String, //To specify what is the file type here .....(Here an image)
+      data: Buffer, //Photo from mongodb, not from cloudinary/AWS S3 -> Stores the binary data of the image
+      contentType: String, // Specifies the type of image file (e.g., JPEG, PNG)
     },
     shipping: {
       type: Boolean, //To show order status
