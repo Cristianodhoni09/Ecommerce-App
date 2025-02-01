b2ea0ae8d1d7de2945f16ba8540ab21c64f804cb
Cristianodhoni09 <amitkum9898@gmail.com>
2025-02-01 23:46:45 +0530

    Added CartContext and CartProvider for managing cart state

---
+++ client/src/context/cart.js
@@ -0,0 +1,24 @@
+import { useState, useContext, createContext, useEffect } from "react";
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    let existingCartItem = localStorage.getItem("cart"); // get existing cart from local storage
+
+    if (existingCartItem) setCart(JSON.parse(existingCartItem)); // parse and set cart
+  }, []);
+
+  return (
+    <CartContext.Provider value={[cart, setCart]}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+// custom hook
+const useCart = () => useContext(CartContext);
+
+export { useCart, CartProvider };
