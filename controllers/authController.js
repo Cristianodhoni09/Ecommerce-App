f5f4c1239fca81327a848657f755b2ecb7cda6d5
Cristianodhoni09 <amitkum9898@gmail.com>
2025-02-04 03:53:28 +0530

    Added getOrdersController to retrieve and display orders for the authenticated user

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 import userModel from "../models/userModel.js";
 import { comparePassword, hashPassword } from "./../helpers/authHelper.js";
 import JWT from "jsonwebtoken";
+import orderModel from "../models/orderModel.js";
 
 export const registerController = async (req, res) => {
   try {
@@ -206,6 +207,26 @@ export const updateProfileController = async (req, res) => {
     res.status(400).send({
       success: false,
       message: "Error while update profile",
+      error,
+    });
+  }
+};
+
+//orders
+export const getOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({ buyer: req.user._id })
+      .populate("products", "-photo")
+      .populate("buyer", "name");
+
+    res.json(orders);
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while geting orders",
       error,
     });
   }
