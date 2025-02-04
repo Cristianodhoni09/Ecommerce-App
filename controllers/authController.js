3c7e84757abe709e9868a07bd78d18fc76a05a44
Cristianodhoni09 <amitkum9898@gmail.com>
2025-02-04 17:15:09 +0530

    Added getAllOrdersController for retrieving all orders and orderStatusController for updating order status

--- controllers/authController.js
+++ controllers/authController.js
@@ -218,7 +218,8 @@ export const getOrdersController = async (req, res) => {
     const orders = await orderModel
       .find({ buyer: req.user._id })
       .populate("products", "-photo")
-      .populate("buyer", "name");
+      .populate("buyer", "name")
+      .sort({createdAt: -1});
 
     res.json(orders);
   } 
@@ -227,6 +228,47 @@ export const getOrdersController = async (req, res) => {
     res.status(500).send({
       success: false,
       message: "Error while geting orders",
+      error,
+    });
+  }
+};
+
+//all orders
+export const getAllOrdersController = async (req, res) => {
+  try {
+    const orders = await orderModel
+      .find({})
+      .populate("products", "-photo")
+      .populate("buyer", "name")
+      .sort({createdAt: -1});
+
+    res.json(orders);
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while geting all orders",
+      error,
+    });
+  }
+};
+
+//order status
+export const orderStatusController = async (req, res) => {
+  try {
+    const { orderId } = req.params;
+    const { status } = req.body;
+
+    const orders = await orderModel.findByIdAndUpdate( orderId, { status }, { new: true } );
+
+    res.json(orders);
+  } 
+  catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while Updating Order status!!",
       error,
     });
   }
