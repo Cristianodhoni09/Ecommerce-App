bbd5aa16651581750fb4138a3eb28ac3332cb104
Cristianodhoni09 <amitkum9898@gmail.com>
2025-02-04 17:15:17 +0530

    Added routes for retrieving all orders and updating order status in authRoute

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,6 +6,8 @@ import {
   forgotPasswordController,
   updateProfileController,
   getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
 } from "../controllers/authController.js";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
@@ -41,6 +43,16 @@ router.put("/profile", requireSignIn, updateProfileController);
 
 //orders
 router.get("/orders", requireSignIn, getOrdersController);
+
+//all orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+// order status update
+router.put("/order-status/:orderId", requireSignIn, isAdmin, orderStatusController);
+
+
+
+
 
 // Add a route for handling 404 errors  (Add all routes before it)
 router.use((req, res) => {
